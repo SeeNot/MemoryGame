25c1d91dd2a780231a3a1dc9fc0e5fb333b0eab2
SeeNot <142214624+SeeNot@users.noreply.github.com>
2025-04-18 20:31:03 +0300

    Working version without points

--- app/src/main/java/com/example/memorygame/ui/CardViewModel.kt
+++ app/src/main/java/com/example/memorygame/ui/CardViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.memorygame.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.memorygame.data.DataSource
 import com.example.memorygame.data.GameState
 import com.example.memorygame.data.MemoryCard
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class CardViewModel : ViewModel() {
 
@@ -56,11 +59,61 @@ class CardViewModel : ViewModel() {
             it.copy(cards = updatedCards, selectedCards = currentSelected)
         }
 
+        if (currentSelected.size >= 2) twoSelectedCards()
 
-        _gameState.update {
-            it.copy(cards = updatedCards)
+    }
+
+    private fun twoSelectedCards() {
+        _gameState.update { it.copy( moves = it.moves + 1) }
+        val selected = _gameState.value.selectedCards
+
+        viewModelScope.launch {
+            val card1 = selected[0]
+            val card2 = selected[1]
+
+            if (card1.imageId == card2.imageId) {
+                delay(1000L)
+                // Match found!
+                val updatedCards = mutableListOf<MemoryCard>()
+                for (card in _gameState.value.cards) {
+                    if (card.imageId != card1.imageId) updatedCards.add(card)
+                }
+
+                _gameState.update {
+                    it.copy(
+                        cards = updatedCards,
+                        selectedCards = emptyList(), // Clear selection
+                    )
+                }
+                // Check for game over after state update
+                hasGameEnded()
+
+            } else {
+                delay(1000L) // Wait for 1 second
+                val updatedCards = _gameState.value.cards.map {
+                    if (it.id == card1.id || it.id == card2.id) {
+                        it.copy(isFaceUp = false) // Flip back down
+                    } else {
+                        it
+                    }
+                }
+                _gameState.update {
+                    it.copy(
+                        cards = updatedCards,
+                        selectedCards = emptyList(), // Clear selection
+                    )
+                }
+            }
+        }
+    }
+
+    private fun hasGameEnded() {
+        if (_gameState.value.cards.isNotEmpty()) {
+            return
         }
 
     }
+
+
 
 }
